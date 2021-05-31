8d1da777880b4591aea782ad05e39e7e27a7c9de
Michael Zargham <mzargham@users.noreply.github.com>
2021-05-31 14:24:19 -0400

    added plotting

--- model/state.py
+++ model/state.py
@@ -2,7 +2,7 @@ from .model.learning import Model
 from .model.items import Item
 
 genesis_state = {
-    "reserve": 100,
+    "reserve": 50,
     "items": {0:Item(0)},
     "model": Model(10) #10 is the number of feature in our attribute space
 }
